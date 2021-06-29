f51c6cb3be3d61e4ac0a0658c949d5cd88c48792
Jelte Jansen <github@tjeb.nl>
2021-06-29 21:09:41 +0200

    Add NL validation rules for CII (UN/CEFACT D16B)
    Test files have been split up into two subdirectories: UBL and CII
    
    The -warnings snippet for UBL was removed to keep the files more clear and to have a single authoritative source file for the validatio
    n rules.

--- validation/generate_xsl.sh
+++ validation/generate_xsl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./convert/convert.sh schematron/peppol-bis-3-with-nl.sch xsl/peppol-bis-3-with-nl.sch
-./convert/convert.sh schematron/peppol-bis-3-with-nl-warnings.sch xsl/peppol-bis-3-with-nl-warnings.sch
+./convert/convert.sh schematron/peppol-bis-3-with-nl-CII.sch xsl/peppol-bis-3-with-nl-CII.sch
